94af46e6a9b20c5a7a3d7b98c61bf217fb6a3ed2
shivaawasthi531 <shivaawasthi531@gmail.com>
2025-12-15 00:26:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-set -e
-
-echo "Building C++ files..."
-
-cd backend/cpp
-
-g++ -std=c++17 main.cpp -o bus_engine
-g++ -std=c++17 metro_engine.cpp -o metro_engine
-
-chmod +x bus_engine metro_engine
-
-echo "Build complete"
+g++ backend/cpp/main.cpp -o backend/cpp/bus_engine
+g++ backend/cpp/metro_engine.cpp -o backend/cpp/metro_engine
+chmod +x backend/cpp/bus_engine backend/cpp/metro_engine
